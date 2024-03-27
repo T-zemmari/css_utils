26bafd52995b3e00095bd1c3734bcace9ace26ef
Tarek <tarek@tedybusiness.com>
2024-03-27 11:24:43 +0100

    BoxShadow:mejorando el css

--- src/Componentes/BoxShadowGenerator/BoxShadowGeneretor.jsx
+++ src/Componentes/BoxShadowGenerator/BoxShadowGeneretor.jsx
@@ -7,11 +7,17 @@ const BoxShadowGeneretor = () => {
   const [blurRadius, setBlurRadius] = useState(10);
   const [spreadRadius, setSpreadRadius] = useState(0);
   const [shadowColor, setShadowColor] = useState("#000000");
+  const [inset, setInset] = useState(false);
 
-  const boxShadowStyle = `${hOffset}px ${vOffset}px ${blurRadius}px ${spreadRadius}px ${shadowColor}`;
+  const generateBoxShadow = () => {
+    const insetValue = inset ? "inset" : "";
+    return `${insetValue} ${hOffset}px ${vOffset}px ${blurRadius}px ${spreadRadius}px ${shadowColor}`;
+  };
+
+  const cssCode = `box-shadow: ${generateBoxShadow()};`;
 
   const handleCopyClick = () => {
-    navigator.clipboard.writeText(`box-shadow: ${boxShadowStyle};`);
+    navigator.clipboard.writeText(cssCode);
     SwAlert("CSS copiado al portapapeles!");
   };
 
@@ -23,56 +29,89 @@ const BoxShadowGeneretor = () => {
             <div className="w-3/6 h-[100%] flex justify-center items-center">
               <div
                 className="w-40 h-40 bg-white border border-gray-300 shadow-lg flex justify-center items-center rounded-lg"
-                style={{ boxShadow: boxShadowStyle }}
+                style={{ boxShadow: generateBoxShadow() }}
               >
                 Preview
               </div>
             </div>
             <div className="w-3/6 flex justify-center items-end">
               <div className="mt-4">
-                <label>Horizontal Offset:</label>
-                <input
-                  type="range"
-                  min="-50"
-                  max="50"
-                  value={hOffset}
-                  onChange={(e) => setHOffset(Number(e.target.value))}
-                />
-                <br />
-                <label>Vertical Offset:</label>
-                <input
-                  type="range"
-                  min="-50"
-                  max="50"
-                  value={vOffset}
-                  onChange={(e) => setVOffset(Number(e.target.value))}
-                />
-                <br />
-                <label>Blur Radius:</label>
-                <input
-                  type="range"
-                  min="0"
-                  max="50"
-                  value={blurRadius}
-                  onChange={(e) => setBlurRadius(Number(e.target.value))}
-                />
-                <br />
-                <label>Spread Radius:</label>
-                <input
-                  type="range"
-                  min="-50"
-                  max="50"
-                  value={spreadRadius}
-                  onChange={(e) => setSpreadRadius(Number(e.target.value))}
-                />
-                <br />
-                <label>Shadow Color:</label>
-                <input
-                  type="color"
-                  value={shadowColor}
-                  onChange={(e) => setShadowColor(e.target.value)}
-                />
-                <br />
+                <div className="w-full flex flex-col">
+                  <label>Horizontal Offset:</label>
+                  <input
+                    type="range"
+                    min="-50"
+                    max="50"
+                    value={hOffset}
+                    onChange={(e) => setHOffset(Number(e.target.value))}
+                  />
+                </div>
+                <div className="w-full flex flex-col">
+                  <label>Vertical Offset:</label>
+                  <input
+                    type="range"
+                    min="-50"
+                    max="50"
+                    value={vOffset}
+                    onChange={(e) => setVOffset(Number(e.target.value))}
+                  />
+                </div>
+                <div className="w-full flex flex-col">
+                  <label>Blur Radius:</label>
+                  <input
+                    type="range"
+                    min="0"
+                    max="50"
+                    value={blurRadius}
+                    onChange={(e) => setBlurRadius(Number(e.target.value))}
+                  />
+                </div>
+                <div className="w-full flex flex-col">
+                  <label>Spread Radius:</label>
+                  <input
+                    type="range"
+                    min="-50"
+                    max="50"
+                    value={spreadRadius}
+                    onChange={(e) => setSpreadRadius(Number(e.target.value))}
+                  />
+                </div>
+                <div className="w-full flex flex-col">
+                  <label>Shadow Color:</label>
+                  <input
+                    type="color"
+                    value={shadowColor}
+                    onChange={(e) => setShadowColor(e.target.value)}
+                  />
+                </div>
+                <div className="mt-4 flex items-center space-x-4">
+                  <button
+                    className={`${
+                      inset ? "bg-gray-300" : "bg-gray-100"
+                    } text-gray-800 py-1 px-4 rounded`}
+                    onClick={() => setInset(false)}
+                  >
+                    Regular
+                  </button>
+                  <button
+                    className={`${
+                      inset ? "bg-gray-100" : "bg-gray-300"
+                    } text-gray-800 py-1 px-4 rounded`}
+                    onClick={() => setInset(true)}
+                  >
+                    Inset
+                  </button>
+                </div>
+                <div className="w-full mt-4 p-4 bg-gray-100">
+                  <label className="block mb-2 font-semibold">
+                    CSS generado:
+                  </label>
+                  <textarea
+                    className="w-full h-24 p-2 border border-gray-300 rounded"
+                    value={cssCode}
+                    readOnly
+                  />
+                </div>
                 <button
                   className="bg-blue-500 text-white px-4 py-2 rounded mt-4"
                   onClick={handleCopyClick}
