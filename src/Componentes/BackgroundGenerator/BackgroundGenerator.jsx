458e6a4527b492238db7f98e82390a8332d8d739
Tarek <tarek@tedybusiness.com>
2024-03-27 16:00:48 +0100

    BackGroundGenerator:preparando la primera vista , la parte izquierda tendra la previsualizacion , y el taller estara en la parte derecha

--- src/Componentes/BackgroundGenerator/BackgroundGenerator.jsx
+++ src/Componentes/BackgroundGenerator/BackgroundGenerator.jsx
@@ -1,11 +1,61 @@
+import { useState, useRef } from "react";
+import { saveSvgAsPng } from "save-svg-as-png";
+import { saveAs } from "file-saver";
+
 const BackgroundGenerator = () => {
-    return (
-      <>
-        <div className="w-full sm:max-w-7xl min-h-screen p-12 mt-10">
-          <span className="text-white">BACKGROUND-GENERATOR</span>
-        </div>
-      </>
-    );
+  const [backgroundColor, setBackgroundColor] = useState("#ffffff");
+  const svgRef = useRef(null);
+
+  const handleExportAsPNG = () => {
+    saveSvgAsPng(svgRef.current, "background.png");
   };
-  
-  export default BackgroundGenerator;
+
+  const handleExportAsSVG = () => {
+    const svgData = new XMLSerializer().serializeToString(svgRef.current);
+    const blob = new Blob([svgData], { type: "image/svg+xml" });
+    saveAs(blob, "background.svg");
+  };
+
+  return (
+    <>
+      <div className="w-full sm:max-w-7xl min-h-screen sm:min-h-[580px] p-2 sm:p-12 sm:mt-36 bg-white flex flex-col-reverse justify-start items-start sm:flex-row sm:rounded-lg">
+        <div
+          className={`contenedor_padre_preview w-full h-full sm:w-3/6 sm:h-[580px] border-x-2 border-indigo-100`}
+        >
+          <div className="contenedor_preview sm:mt-8">
+            <img src="" alt="" />
+          </div>
+        </div>
+        <div
+          className={`contenedor_padre_taller w-full sm:w-3/6 h-[580px]  mt-24 sm:mt-0 p-2 sm:p-0 sm:border-l-2`}
+        >
+          <div className="w-full h-[85%] flex justify-start items-start border-y-2 border-indigo-100">
+          
+          </div>
+          <div className="w-full h-[15%] flex justify-end sm:p-2">
+            <button
+              className="bg-blue-500 text-white px-4 py-2 rounded mr-4"
+              onClick={handleExportAsPNG}
+            >
+              Exportar PNG
+            </button>
+            <button
+              className="bg-green-500 text-white px-4 py-2 rounded mr-4"
+              onClick={handleExportAsSVG}
+            >
+              Exportar SVG
+            </button>
+            <button
+              className="bg-red-500 text-white px-4 py-2 rounded"
+              onClick={handleExportAsSVG}
+            >
+              Previsualizar
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BackgroundGenerator;
