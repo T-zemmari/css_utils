c8c9fcd6d5bbe62b54c3696132b377096ec26292
guirep <guirep@gmail.com>
2024-03-29 13:06:31 +0100

    WaveComponent : dibujar las ondas horizontalmente o verticalmente dependiento del valor renderWave pasado por el padre

--- src/Componentes/WaveComponent/WaveComponent.jsx
+++ src/Componentes/WaveComponent/WaveComponent.jsx
@@ -1,20 +1,21 @@
 import React, { useEffect } from "react";
 
-const WaveComponent = ({ initialColor, waveHeight, waveCount,renderWave }) => {
-  const viewBoxHeight = waveHeight * waveCount;
+const WaveComponent = ({ initialColor, waveHeight, waveCount, renderWave }) => {
+  const viewBoxWidth = renderWave === "horizontal" ? 100 * waveCount : 100;
+  const viewBoxHeight = renderWave === "horizontal" ? waveHeight : waveHeight * waveCount;
 
   useEffect(() => {
     console.log("initialColor", initialColor);
     console.log("waveHeight", waveHeight);
     console.log("waveCount", waveCount);
     console.log("renderWave", renderWave);
-  }, [initialColor,initialColor,waveCount,renderWave]);
+  }, [initialColor, waveHeight, waveCount, renderWave]);
 
   return (
     <svg
       width="100%"
       height="100%"
-      viewBox={`0 0 100 ${viewBoxHeight}`}
+      viewBox={`0 0 ${viewBoxWidth} ${viewBoxHeight}`}
       preserveAspectRatio="none"
       style={{ position: "absolute", bottom: 0, left: 0, zIndex: -1 }}
     >
@@ -25,12 +26,25 @@ const WaveComponent = ({ initialColor, waveHeight, waveCount,renderWave }) => {
           <stop offset="100%" stopColor={initialColor} />
         </linearGradient>
       </defs>
-      <path
-        d={`M 0 ${waveHeight} Q 25 ${
-          waveHeight * 1.5
-        }, 50 ${waveHeight} T 100 ${waveHeight} V ${viewBoxHeight} H 0 Z`}
-        fill="url(#waveGradient)"
-      />
+      {/* Dibujar las olas */}
+      {renderWave === "horizontal" ? (
+        Array.from({ length: waveCount }).map((_, index) => (
+          <path
+            key={index}
+            d={`M ${index * 100} ${waveHeight} Q ${index * 100 + 25} ${
+              waveHeight * 1.5
+            }, ${index * 100 + 50} ${waveHeight} T ${
+              index * 100 + 100
+            } ${waveHeight} V ${viewBoxHeight} H ${index * 100} Z`}
+            fill="url(#waveGradient)"
+          />
+        ))
+      ) : (
+        <path
+          d={`M 0 ${waveHeight} Q 25 ${waveHeight * 1.5}, 50 ${waveHeight} T 100 ${waveHeight} V ${viewBoxHeight} H 0 Z`}
+          fill="url(#waveGradient)"
+        />
+      )}
     </svg>
   );
 };
