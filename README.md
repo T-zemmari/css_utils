657523f55f00d794c5b64235a266e3378df156af
Tarek <tarek@tedybusiness.com>
2024-04-02 08:40:54 +0200

    maquetando el la landig page, usando un background genrado por la misma app, tambien se ha modificado el readme

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# CSS-UTILS
+# CSS Utils App
 
+CSS Utils es una aplicación que proporciona herramientas para generar propiedades CSS, como sombras de caja y fondos ondulados.
+
+## Funcionalidades
+
+- **Box Shadow Generator:** Genera sombras de caja personalizadas utilizando el componente Box Shadow Generator.
+- **Background Generator:** Crea fondos ondulados ajustables con el componente Background Generator.
+
+## Cómo usar
+
+1. Clona este repositorio en tu máquina local.
+2. Instala las dependencias con `npm install`.
+3. Ejecuta la aplicación con `npm start`.
+4. Navega a través de las diferentes herramientas utilizando la barra de navegación.
+5. Personaliza las propiedades según tus necesidades y genera el código CSS correspondiente.
+
+¡Disfruta de la experiencia de generación de CSS con CSS Utils App!
--- src/Componentes/LandingPage/LandingPage.jsx
+++ src/Componentes/LandingPage/LandingPage.jsx
@@ -3,6 +3,8 @@ import shadowsJson from "../../shadows.json";
 import { useEffect, useState } from "react";
 import SwAlert from "../SwAlert/SwAlert";
 import ScrollToTopButton from "../ButtonScrollToTop/ButtonScrollToTop";
+import { Link } from "react-router-dom";
+import bgOneSvg from "../../assets/svgs/background_2.svg";
 
 const LandingPage = () => {
   const [boxSelected, setBoxselected] = useState("");
@@ -33,13 +35,59 @@ const LandingPage = () => {
     SwAlert(`Css copiado correctamente`);
   };
 
+  const handleClickGoToBoxShadows=(elementId)=>{
+     let element=document.getElementById(elementId);
+     if(element){
+      element.scrollIntoView({
+        behavior:'smooth',
+      })
+     }
+  }
+
   return (
     <>
       <ScrollToTopButton />
+      <div className="w-full min-h-screen flex flex-col justify-center items-start p-12"
+          style={{
+            backgroundImage: `url(${bgOneSvg})`,
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+            backgroundSize: "contain"
+          }}>
+          <h1 className="text-8xl font-bold mb-6 text-gray-900">
+            CSS Utils App
+          </h1>
+          <p className="text-lg text-gray-700 mb-8">
+            ¡Bienvenido a CSS Utils! Esta aplicación proporciona herramientas
+            para generar propiedades CSS como sombras de caja y fondos
+            ondulados.
+          </p>
+          <div className="flex gap-4">
+            <Link
+              to="/box-shadow-generator"
+              className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+            >
+             Generador Box Shadow 
+            </Link>
+            <Link
+              to="/background-generator"
+              className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
+            >
+              Generador Background 
+            </Link>
+          </div>
+          <div className="flex justify-center items-center gap-4">
+            <button className="w-[150px] h-[150px] rounded-full bg-[#ffb4b9] mt-10 text-white font-bold" onClick={()=>handleClickGoToBoxShadows('box_shadows_container')}>BOXSHADOWS</button>
+        
+          </div>
+        </div>
       <div className="w-full max-w-7xl min-h-screen p-2 sm:p-12 flex flex-col items-center">
-        <h1 className="text-[#71731f] text-[36px] sm:text-[56px] font-bold mt-[80px] sm:mt-12">
+       
+        <hr />      
+          <h1 className="text-[#71731f] text-[36px] sm:text-[56px] font-bold mt-[80px] sm:mt-12" id="box_shadows_container">
           BOX-SHADOWS
         </h1>
+
         <div className="w-full h-[100%] bg-white mt-4 flex flex-col justify-start items-start rounded-lg">
           <div className="w-full min-h-[180px] flex flex-col justify-center items-center p-10">
             <h2 className="text-2xl mb-10 font-bold">SOFT-SHADOWS</h2>
@@ -111,7 +159,7 @@ const LandingPage = () => {
         <div className="w-full h-[100%] bg-white mt-4 flex flex-col justify-start items-start rounded-lg">
           <div className="w-full min-h-[180px] flex flex-col justify-center items-center p-10">
             <h2 className="text-2xl mb-10 font-bold">HOVERD-SHADOWS</h2>
-            <div className="grid grid-cols-1 sm:grid-cols-4 gap-10 sm:gap:20">
+            <div className="grid grid-cols-1 sm:grid-cols-4 gap-10 sm:gap:20" >
               {hoverShadows?.map((item) => (
                 <ButtonShadow
                   key={item.id}
