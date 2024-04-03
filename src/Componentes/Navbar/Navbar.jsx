ad7dfe2417004ee964e66c8e802b0e85a3339a7a
Tarek <tarek@tedybusiness.com>
2024-04-03 16:00:32 +0200

    README

--- src/Componentes/Navbar/Navbar.jsx
+++ src/Componentes/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Logo from "../../assets/logos/png/logo_hr_2.png";
+import Logo from "../../assets/logos/png/logo_hr_6.png";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
@@ -7,7 +7,7 @@ const Navbar = () => {
 
   return (
     <nav className="fixed top-0 w-full bg-[#1634a7] z-[10000000]">
-      <div className="max-w-7xl mx-auto px-4">
+      <div className="mx-auto px-4">
         <div className="flex justify-between h-16">
           <div className="w-full flex justify-between">
             <div className="flex items-center">
